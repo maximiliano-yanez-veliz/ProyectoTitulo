98809df9d443e2498aed9c2876ad0fe7033e3260
Maximiliano Yañez Veliz <max.yanez.veliz@gmail.com>
2022-09-09 15:26:25 -0400

    Mejora completa planilla ejercicios

--- PlanillaEjercicios/admin.py
+++ PlanillaEjercicios/admin.py
@@ -2,10 +2,29 @@ from pydoc import ModuleScanner
 from django.contrib import admin
 
 from PlanillaEjercicios.models import Ejercicios
-from .models import Ejercicios
+from .models import Ejercicios, Habilidades, HabilidadEjercicio
 from django.contrib import admin
 
+class HabilidadEjercicioInline(admin.TabularInline):
+    model = HabilidadEjercicio
+    extra = 1
+    autocomplete_fields = ['habilidades']
+
+class HabilidadesAdmin(admin.ModelAdmin):
+    inlines = (HabilidadEjercicioInline,)
+    search_fields = ('hablidad'),
+    ordering = ['hablidad']
+
+class EjercicioAdmin(admin.ModelAdmin):
+    inlines = [HabilidadEjercicioInline,]
+    list_display = (
+        'titulo',
+        'subtitulo',
+        'descripcion',
+    )
+    #
 
 
 # Register your models here.
-admin.site.register(Ejercicios)
+admin.site.register(Habilidades, HabilidadesAdmin)
+admin.site.register(Ejercicios,EjercicioAdmin)
--- PlanillaEjercicios/models.py
+++ PlanillaEjercicios/models.py
@@ -1,10 +1,51 @@
 from django.db import models
 
+class Habilidades(models.Model):
+    hablidad = models.CharField('Habilidad', max_length=200)
 
-# Create your models here.
+    class meta:
+        verbose_name = 'Habilidad'
+        verbose_name_plural = 'Habilidades Ejercicio'
+    
+    def __str__(self):
+        return str(self.id) + '-' + self.hablidad
 
 class Ejercicios(models.Model):
-    titulo = models.CharField(max_length=50)
-    subtitulo = models.CharField(max_length=50 )
+    
+
+    titulo = models.CharField('Titulo', max_length=50)
+    subtitulo = models.CharField('Subtitulo', max_length=50 )
     descripcion = models.TextField('Descripcion')
     
+    habilidades = models.ManyToManyField(
+    Habilidades,
+    through='HabilidadEjercicio',
+    blank=True,
+    )
+
+
+class HabilidadEjercicio(models.Model):
+    description = models.CharField(
+        'titulo',
+        max_length=100,
+        blank=True,
+    )
+    habilidades = models.ForeignKey(
+        Habilidades, 
+        on_delete=models.CASCADE,
+        blank=True, null=True
+    )
+    ejercicio = models.ForeignKey(
+        Ejercicios, 
+        on_delete=models.CASCADE,
+        blank=True, null=True
+    )
+    
+    class meta:
+        db_table = 'ejercicio_ejercicio_habilidades'
+        verbose_name = 'Habilidad Ejercicio'
+        verbose_name_plural = 'Habilidades Ejercicios'
+
+
+
+
