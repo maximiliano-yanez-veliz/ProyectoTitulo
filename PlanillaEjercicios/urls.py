2d7df01306ebe222b5914562ebd261a520683e20
Maximiliano Yañez Veliz <max.yanez.veliz@gmail.com>
2022-09-21 00:34:40 -0300

    HTML-DJANGO

--- PlanillaEjercicios/urls.py
+++ PlanillaEjercicios/urls.py
@@ -1,10 +1,1 @@
 from django.urls import path
-from .views import EjercicioDetalladoView, EjerciciosListView
-
-
-urlpatterns = [
-    path('ejercicios/', EjerciciosListView.as_view(), name='ejercicios_list'),
-    path('ejercicios/<int:pk>', EjercicioDetalladoView.as_view(), name='ejercicios_process')
-     
-    
-]
--- PlanillaEjercicios/views.py
+++ PlanillaEjercicios/views.py
@@ -1,55 +1,8 @@
-from ast import Delete
-from django.views import View
-from django.views.decorators.csrf import csrf_exempt, ensure_csrf_cookie
-from .models import Ejercicios
-from django.http import JsonResponse
-from django.utils.decorators import method_decorator
-import json
-from django.forms.models import model_to_dict
-from django.shortcuts import get_object_or_404
 
-# Create your views here.
-class EjerciciosListView(View) :
-    
-    @method_decorator(csrf_exempt)
-    def dispatch(self, request, *args, **kwargs):
-        return super().dispatch(request, *args, **kwargs)
+# from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Habilidades
 
-
-    def get(self, request):
-        cList = Ejercicios.objects.all ()
-        return JsonResponse(list(cList.values()), safe=False)
-
-
-    def post(self, request) :
-        #print(request.body)
-        jd = json.loads(request.body)
-        #print(jd)
-        Ejercicios.objects.create(titulo=jd['titulo'], subtitulo=jd['subtitulo'] ,descripcion=jd['descripcion'])
-        datos = {'message': "Success"}
-        return JsonResponse(datos)
-         
-class EjercicioDetalladoView(View):
-    @method_decorator(csrf_exempt)
-    def dispatch(self, request, *args, **kwargs):
-        return super().dispatch(request, *args, **kwargs)
-
-    def get(self, request, pk):
-        list = Ejercicios.objects.get(pk=pk)
-        return JsonResponse (model_to_dict(list))
-
-    def put(self, request, pk) :
-            jd=json.loads(request.body)
-            obj = get_object_or_404(Ejercicios, id = pk)
-            obj.titulo=jd['titulo']
-            obj.subtitulo=jd['subtitulo']
-            obj.descripcion=jd['descripcion']
-            obj.save()
-            datos = {'message': "Success"}
-            return JsonResponse(datos)
-
-    def delete(self, request, pk):
-            obj = get_object_or_404(Ejercicios, id = pk)
-            obj.delete()
-            datos = {'message': "Success"}
-            return JsonResponse(datos)
+def home(request):
+    habilidListado = Habilidades.objects.all()
+    return render(request,"gestionEjercicios.html",{"jercicio": habilidListado})
