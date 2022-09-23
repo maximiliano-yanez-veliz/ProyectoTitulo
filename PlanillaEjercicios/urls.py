dd1c77068e02eb5f4c9123116ab949a2b0eac4fd
Maximiliano Yañez Veliz <max.yanez.veliz@gmail.com>
2022-09-23 17:01:44 -0300

    avance

--- PlanillaEjercicios/urls.py
+++ PlanillaEjercicios/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
-from PlanillaEjercicios.views import EjercicioListView, registrar_ejercicio, eliminar_ejercicio
+from PlanillaEjercicios.views import EjercicioListView, registrar_ejercicio, eliminar_ejercicio, edicion_ejercicio, editar_ejercicio, registro_ejercicio, creacion_ejercicio, habilidad_ejercicios
 
 urlpatterns = [
     path('', EjercicioListView.as_view(), name='gestion_ejercicios'),
     path('eliminacionEjercicios/<int:id>', eliminar_ejercicio),
-    path('registrarEjercicio/', registrar_ejercicio)
+    path('registrarEjercicio/', registrar_ejercicio),
+    path('edicionEjercicios/<int:id>', edicion_ejercicio),
+    path('editarEjercicio/', editar_ejercicio),
+    path('registroEjercicio/', registro_ejercicio),
+    path('creacionEjercicio/', creacion_ejercicio),
+    path('habilidadEjercicios/', habilidad_ejercicios),
+    
 ]
 
--- PlanillaEjercicios/views.py
+++ PlanillaEjercicios/views.py
@@ -3,7 +3,7 @@
 from msilib.schema import ListView
 from pyexpat import model
 from django.shortcuts import render, redirect
-from .models import Ejercicios
+from .models import Ejercicios, HabilidadEjercicio, Habilidades
 from django.views.generic import ListView
 
 def home(request):
@@ -34,16 +34,64 @@ def registrar_ejercicio(request):
     ejercicio=Ejercicios.objects.create(titulo=titulo, descripcion=descripcion, dificultad=dificultad)
     return redirect('/')
 
-
-
-    
 def eliminar_ejercicio(request,id):
     ejercicio = Ejercicios.objects.get(id=id)
     ejercicio.delete()
     return redirect('/')
 
+def edicion_ejercicio(request,id):
+    ejercicio = Ejercicios.objects.get(id=id)
+    data = {
+        'titulo':'Edicion de Ejercicios',
+        'ejercicio' : ejercicio
 
+    }
+    return render(request,"edicionEjercicios.html", data)
 
+def editar_ejercicio(request):
+    id =int(request.POST['id'])
+    titulo=request.POST['txtTitulo']
+    descripcion=request.POST['txtDescripcion']
+    dificultad=request.POST['numDificultad']
+    
+    ejercicio = Ejercicios.objects.get(id=id)
+    ejercicio.titulo=titulo
+    ejercicio.descripcion=descripcion
+    ejercicio.dificultad=dificultad
+    ejercicio.save()
+    
+    return redirect('/')
 
+def registro_ejercicio(request):
+    data = {
+        'titulo':'Edicion de Ejercicios',
 
+    }
+    return render(request,"registroEjercicio.html")
 
+def creacion_ejercicio(request):
+    titulo=request.POST['txtTitulo']
+    descripcion=request.POST['txtDescripcion']
+    dificultad=request.POST['numDificultad']
+    
+    ejercicio=Ejercicios.objects.create(titulo=titulo, descripcion=descripcion, dificultad=dificultad)
+    return redirect('/')
+
+def habilidad_ejercicios(request):
+    habilidListado = Habilidades.objects.all()
+    
+    data = {
+        'titulo':'Gestion de Habilidades',
+        'jercicio' : habilidListado
+
+    }
+    # return render(request,"gestionEjercicios.html",{"jercicio": habilidListado})
+    return render(request,"habilidadesEjercicios.html", data)
+class HabilidadesListView(ListView):
+    model = Habilidades
+    template_name = 'gestionEjercicios.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['titulo'] = 'Gestion de Ejercicios'
+        return context
